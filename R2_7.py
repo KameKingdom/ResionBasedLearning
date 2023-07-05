b7902debd7813decd18d65fc7c930718fa480ed0
KameKingdom <117248688+KameKingdom@users.noreply.github.com>
2023-07-05 15:56:20 +0900

    Upload R2_7.py

---
+++ R2_7.py
@@ -0,0 +1,103 @@
+yolo_dict = {
+    0: "person",
+    1: "bicycle",
+    2: "car",
+    3: "motorcycle",
+    4: "airplane",
+    5: "bus",
+    6: "train",
+    7: "truck",
+    8: "boat",
+    9: "traffic light",
+    10: "fire hydrant",
+    11: "stop sign",
+    12: "parking meter",
+    13: "bench",
+    14: "bird",
+    15: "cat",
+    16: "dog",
+    17: "horse",
+    18: "sheep",
+    19: "cow",
+    20: "elephant",
+    21: "bear",
+    22: "zebra",
+    23: "giraffe",
+    24: "backpack",
+    25: "umbrella",
+    26: "handbag",
+    27: "tie",
+    28: "suitcase",
+    29: "frisbee",
+    30: "skis",
+    31: "snowboard",
+    32: "sports ball",
+    33: "kite",
+    34: "baseball bat",
+    35: "baseball glove",
+    36: "skateboard",
+    37: "surfboard",
+    38: "tennis racket",
+    39: "bottle",
+    40: "wine glass",
+    41: "cup",
+    42: "fork",
+    43: "knife",
+    44: "spoon",
+    45: "bowl",
+    46: "banana",
+    47: "apple",
+    48: "sandwich",
+    49: "orange",
+    50: "broccoli",
+    51: "carrot",
+    52: "hot dog",
+    53: "pizza",
+    54: "donut",
+    55: "cake",
+    56: "chair",
+    57: "couch",
+    58: "potted plant",
+    59: "bed",
+    60: "dining table",
+    61: "toilet",
+    62: "tv",
+    63: "laptop",
+    64: "mouse",
+    65: "remote",
+    66: "keyboard",
+    67: "cell phone",
+    68: "microwave",
+    69: "oven",
+    70: "toaster",
+    71: "sink",
+    72: "refrigerator",
+    73: "book",
+    74: "clock",
+    75: "vase",
+    76: "scissors",
+    77: "teddy bear",
+    78: "hair drier",
+    79: "toothbrush"
+}
+
+import zipfile
+
+path = "C:/Users/yudai/Desktop/KGU/3年生春学期/領域実習/ResionBasedLearning1/ResionBasedLearning1/labels.zip"
+objects = []
+numbers = []
+
+with zipfile.ZipFile(path, 'r') as zip_file:
+    for file_name in zip_file.namelist():
+        with zip_file.open(file_name, 'r') as file:
+            data = file.read().decode("utf-8")
+            data = data.split("\n")
+            for line in data:
+                line = line.split(' ')
+                if line[0]:
+                    numbers.append(line[0])
+            for i in numbers:
+                objects.append(yolo_dict[int(i)])
+            print(numbers)
+
+print(f"detect: {set(objects)}")
